2429f1545a91b8e1e55b7cb4eacdf97b34f513ff
Marcos-zgz <mabad350@alumno.uned.es>
2026-02-01 14:30:25 +0100

    Update quien-soy.jsx

--- pages/quien-soy.jsx
+++ pages/quien-soy.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Head from 'next/head';
 import Nav from "../components/nav";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function QuienSoy() {
   return (
